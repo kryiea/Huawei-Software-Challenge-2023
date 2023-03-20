51aed7928558bf943da77bb9966fa47b94d1d2b7
kryiea <1582877056@qq.com>
2023-03-21 02:56:20 +0800

    交互解决

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <cstdio>
-#include <cstdlib>
-#include <unordered_map>
-#include <fstream>
+#include <vector>
 
 using namespace std;
 
-//char map[105][105]{};
 char map[1024]{};
 int frame_ID{}; // 帧ID
-long int money{};
-unordered_map<string, string> robotOrder;
+long int money{}; // 当前金钱
+vector<string> robotOrder{};//机器人指令集
 
-
+// 机器人类
 struct robot{
     int workbench_ID{}; // -1：不处于工作台附近 [0-8]: 工作台按顺序排序，0开始
     int item_ID{}; //携带的物品ID，[0,7]:  0 表示未携带物品
@@ -25,6 +22,7 @@ struct robot{
     float position_X{};//浮点坐标x y
     float position_Y{};//浮点坐标x y
 }robot[5];
+//工作台类
 struct workbench{
     int ID{}; //[1,9]
     float position_X{};//浮点坐标x y
@@ -32,49 +30,45 @@ struct workbench{
     int time_prodRemaining{}; //-1：表示没有生产。0：表示生产因输出格满而阻塞。>=0：表示剩余生产帧数。
     int status_rawGrid{}; //二进制位表描述，例如 48(110000) 表示拥有物品 4 和 5。
     int status_prodGrid{}; //0：表示无。1：表示有。
-    int sum_workbench{};
-}workbench[10];
+    int sum_workbench{}; //当前地图工作台总数 存在workbench[0]中
+}workbench[51];
 
-bool readMap(); // 读取地图信息
-bool Print_robotOrder(unordered_map<string, string> robotOrder); //输出当前帧的指令集，Ok换行结束
+bool initMap(); // 读取地图信息
+bool Print_robotOrder(); //输出当前帧的指令集，Ok换行结束
 bool readFrameData();// 获取帧信息
 
 int main() {
-    readMap();
+    initMap();
     puts("OK");
-    fflush(stdout);
+    //fflush(stdout);
+    cout << flush;
+
+    //测试操作
+    robotOrder.push_back("forward 0 4");
+    robotOrder.push_back("rotate 0 3.14159");
+    robotOrder.push_back("forward 1 4");
+    robotOrder.push_back("rotate 0 3.14159");
+    robotOrder.push_back("forward 2 4");
+    robotOrder.push_back("rotate 0 3.14159");
+    robotOrder.push_back("forward 3 4");
+    robotOrder.push_back("rotate 3 3.14159");
 
     while(scanf("%d",&frame_ID) != EOF){
-        //printf("%d\n", frame_ID);
-
         //读入帧信息
         readFrameData();
 
-        ofstream ofs;
-        ofs.open("output.txt",ios::app);
-        for (int j = 0; j < 7; ++j) {
-            ofs << robot[j].workbench_ID << " " << robot[j].item_ID<< " "
-                << robot[j].time_Val << " "<< robot[j].crash_Val<< " "
-                << robot[j].angleSpeed<< " "
-                << robot[j].lineSpeed_X<< " " << robot[j].lineSpeed_Y<< " "
-                << robot[j].angle<< " "
-                << robot[j].position_X << " "<< robot[j].position_Y<< " ";
-        }
-         ofs.close();
         //规划函数
-        printf("OK\n");
-        fflush(stdout);
-    }
 
+        //输出指令
+        Print_robotOrder();
+    }
     return 0;
 }
 
 
 /**
   * @brief          : 读取帧信息
-  * @note           : 
-  * @param          : 
-  * @retval         : 
+  * @retval         :
 */
 bool readFrameData() {
     cin >> money;
@@ -87,7 +81,7 @@ bool readFrameData() {
         cin >> workbench[i].position_X >> workbench[i].position_Y;
         cin >> workbench[i].time_prodRemaining >> workbench[i].status_rawGrid >> workbench[i].status_prodGrid;
     }
-    while(i <= workbench[0].sum_workbench);
+    while(i < workbench[0].sum_workbench);
 
     //读取机器人数据
     for (int j = 0; j < 4; ++j) {
@@ -99,7 +93,6 @@ bool readFrameData() {
             >> robot[j].position_X >> robot[j].position_Y;
     }
 
-
     //读取OK
     string s;
     cin >> s;
@@ -107,29 +100,40 @@ bool readFrameData() {
     return false;
 }
 
+
+
 /**
   * @brief          : 读取当前地图数据
-  * @note           : 
-  * @param          :
-  * @retval         : 
+  * @retval         :
 */
-bool readMap() {
+bool initMap() {
     while (fgets(map, sizeof map, stdin)) {
         if (map[0] == 'O' && map[1] == 'K') {
             return true;
         }
         //do something
+
     }
     return false;
 }
 
 
+
+
 /**
   * @brief          : 输出操作指令
-  * @note           :
-  * @param          :
   * @retval         :
 */
-bool Print_robotOrder(unordered_map<string, string> robotOrder){
+bool Print_robotOrder(){
+    //输出帧ID
+    cout << frame_ID << endl;
+    //指令map
+    for (auto & iter : robotOrder) {
+        cout << iter << endl;
+    }
+    //输出OK
+    printf("OK\n");
+    //刷新stream流
+    fflush(stdout);
     return true;
 }
